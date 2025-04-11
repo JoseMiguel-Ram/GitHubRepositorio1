b43378aaf151cc3880140a6866d3c70149775d83
José Ramírez <suarezjose200314@gmail.com>
2025-04-11 00:16:26 -0600

    Otra salida distinta desde master

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ Poblacion = ordenado.Values.ToArray ();
 //Mostrar los arreglos ordenados de menor a mayor.
 for (var i= 0; i< Poblacion. Length; i++)
 Console.WriteLine ($"{Departamento [i],20} ==> {Poblacion [i],10:N0}");
- //Suma de toda la población y nombre de mayor y menor 
+ //suma de todas las problaciones de mayor a menor 
 Console.WriteLine ($" Población General:{Poblacion. Sum():N0}");
 Console.WriteLine ($"Departamento con mayor Población:{maxDepkey}");
 Console.WriteLine ($"Departamento con menor Población:{minDepkey}");
